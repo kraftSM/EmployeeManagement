60ad18bf5380b326a5139d6535de34ee2bd772d1
kraftsm <kraftsm@yandex.ru>
2023-09-12 16:43:07 +0300

    Добавьте файлы проекта.

---
+++ EmployeeManagement/View/EmployessView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeeManagement.View
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployessView.xaml
+    /// </summary>
+    public partial class EmployessView : Window
+    {
+        public EmployessView()
+        {
+            InitializeComponent();
+        }
+    }
+}
